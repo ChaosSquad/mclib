6b62ffafb0cb9b8871e33b66ada90bb7fc08896c
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2024-04-23 16:04:52 +0200

    added TabCompletingCommandExecutor

--- src/main/java/net/chaossquad/mclib/command/SubcommandEntry.java
+++ src/main/java/net/chaossquad/mclib/command/SubcommandEntry.java
@@ -4,4 +4,13 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.TabCompleter;
 
 public record SubcommandEntry(CommandExecutor executor, TabCompleter tabCompleter, String permission) {
+
+    public static SubcommandEntry of(TabCompletingCommandExecutor command, String permission) {
+        return new SubcommandEntry(command, command, permission);
+    }
+
+    public static SubcommandEntry of(TabCompletingCommandExecutor command) {
+        return new SubcommandEntry(command, command, null);
+    }
+
 }
---
+++ src/main/java/net/chaossquad/mclib/command/TabCompletingCommandExecutor.java
@@ -0,0 +1,7 @@
+package net.chaossquad.mclib.command;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabCompleter;
+
+public interface TabCompletingCommandExecutor extends CommandExecutor, TabCompleter {
+}
