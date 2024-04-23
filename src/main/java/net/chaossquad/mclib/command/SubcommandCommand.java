b2331b577b5cd13bd8f7d798d9ac8bc63be9c8fd
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2024-04-23 17:59:27 +0200

    added DynamicSubcommandProvider

--- src/main/java/net/chaossquad/mclib/command/SubcommandCommand.java
+++ src/main/java/net/chaossquad/mclib/command/SubcommandCommand.java
@@ -7,15 +7,24 @@ import org.bukkit.plugin.Plugin;
 
 import java.util.*;
 
+/**
+ * A command that can has multiple commands as subcommands.
+ */
 public class SubcommandCommand implements TabCompletingCommandExecutor {
     private final Plugin plugin;
     private final Map<String, SubcommandEntry> entries;
+    private final DynamicSubcommandProvider dynamicSubcommandProvider;
     private String availableSubcommandsMessage;
 
-    public SubcommandCommand(Plugin plugin, String availableSubcommandsMessage) {
+    public SubcommandCommand(Plugin plugin, DynamicSubcommandProvider dynamicSubcommandProvider, String availableSubcommandsMessage) {
         this.plugin = plugin;
         this.entries = new HashMap<>();
+        this.dynamicSubcommandProvider = dynamicSubcommandProvider;
         this.availableSubcommandsMessage = Objects.requireNonNullElse(availableSubcommandsMessage, "");
+    }
+
+    public SubcommandCommand(Plugin plugin, String availableSubcommandsMessage) {
+        this(plugin, null, availableSubcommandsMessage);
     }
 
     // COMMAND
@@ -25,7 +34,7 @@ public class SubcommandCommand implements TabCompletingCommandExecutor {
 
         if (args.length > 0) {
 
-            SubcommandEntry subcommand = this.entries.get(args[0]);
+            SubcommandEntry subcommand = this.getSubcommand(args[0]);
             if (subcommand == null) {
                 sender.sendMessage("§cUnknown subcommand");
                 return true;
@@ -71,7 +80,7 @@ public class SubcommandCommand implements TabCompletingCommandExecutor {
 
         if (args.length > 1) {
 
-            SubcommandEntry subcommand = this.entries.get(args[0]);
+            SubcommandEntry subcommand = this.getSubcommand(args[0]);
             if (subcommand == null) return List.of();
             if (sender != this.plugin.getServer().getConsoleSender() && subcommand.permission() != null && sender.hasPermission(subcommand.permission())) return List.of();
 
@@ -102,6 +111,16 @@ public class SubcommandCommand implements TabCompletingCommandExecutor {
 
     }
 
+    private SubcommandEntry getSubcommand(String name) {
+
+        SubcommandEntry entry = null;
+
+        if (this.dynamicSubcommandProvider != null) entry = this.dynamicSubcommandProvider.getDynamicSubcommands().get(name);
+        if (entry == null) this.entries.get(name);
+
+        return entry;
+    }
+
     private List<String> getSubcommandList(CommandSender sender) {
 
         if (sender == null) return List.of();
@@ -118,39 +137,89 @@ public class SubcommandCommand implements TabCompletingCommandExecutor {
 
         }
 
+        if (this.dynamicSubcommandProvider != null) {
+            for (String command : List.copyOf(this.dynamicSubcommandProvider.getDynamicSubcommands().keySet())) {
+                SubcommandEntry entry = this.entries.get(command);
+                if (entry == null) continue;
+
+                if (sender == this.plugin.getServer().getConsoleSender() || entry.permission() == null || sender.hasPermission(entry.permission())) {
+                    commandList.add(command);
+                }
+
+            }
+        }
+
         return commandList;
 
     }
 
     // MANAGE SUBCOMMANDS
 
+    /**
+     * Adds a subcommand.
+     * @param command command string
+     * @param data command data
+     */
     public void addSubcommand(String command, SubcommandEntry data) {
         this.entries.put(command, data);
     }
 
+    /**
+     * Removes a subcommand.
+     * @param command command string
+     */
     public void removeSubcommand(String command) {
         this.entries.remove(command);
     }
 
+    /**
+     * Clears all subcommands.
+     */
     public void clearSubcommands() {
         this.entries.clear();
     }
 
-    // GETTER
+    // GETTER / SETTER
 
+    /**
+     * Returns the plugin.
+     * @return plugin
+     */
     public Plugin getPlugin() {
         return this.plugin;
     }
 
+    /**
+     * Returns a map of all registered subcommands.
+     * @return map of subcommands
+     */
     public Map<String, SubcommandEntry> getSubcommands() {
         return Map.copyOf(this.entries);
     }
 
+    /**
+     * Returns the dynamic subcommand provider.
+     * Read {@link DynamicSubcommandProvider} for more information.
+     * @return dynamic subcommand provider
+     */
+    public DynamicSubcommandProvider getDynamicSubcommandProvider() {
+        return this.dynamicSubcommandProvider;
+    }
+
+    /**
+     * Get message that is displayed when the user runs the command without arguments.
+     * @return available subcommands message
+     */
     public String getAvailableSubcommandsMessage() {
         return availableSubcommandsMessage;
     }
 
+    /**
+     * Set message that is displayed when the user runs the command without arguments.
+     * @param availableSubcommandsMessage available subcommands message
+     */
     public void setAvailableSubcommandsMessage(String availableSubcommandsMessage) {
         this.availableSubcommandsMessage = Objects.requireNonNullElse(availableSubcommandsMessage, "");
     }
+
 }
