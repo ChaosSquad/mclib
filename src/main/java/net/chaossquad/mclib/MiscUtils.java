c3b3bfc92fec5e571c583d163e715e5b151cf3cf
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-01-05 22:31:44 +0100

    MiscUtils#getAspectOfTheWorld

--- src/main/java/net/chaossquad/mclib/MiscUtils.java
+++ src/main/java/net/chaossquad/mclib/MiscUtils.java
@@ -1,6 +1,17 @@
 package net.chaossquad.mclib;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Registry;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
@@ -237,6 +248,74 @@ public final class MiscUtils {
         }
 
         return locations;
+    }
+
+    /**
+     * Returns the sword "Aspect of the World".<br/>
+     * This is a sword we have found on mcstacker saved commands a long time ago.<br/>
+     * We now have updated it to the newer versions.<br/>
+     * See: <a href="https://gist.github.com/jandie1505/29cfc6b3ee7ab46e3df3dae5b4bcd848">Command version</a><br/>
+     * @return aspect of the world item
+     */
+    public static ItemStack getAspectOfTheWorld() {
+        ItemStack item = new ItemStack(Material.GOLDEN_SWORD);
+        ItemMeta meta = item.getItemMeta();
+
+        meta.displayName(Component.text("Aspect of the World").color(NamedTextColor.DARK_RED).decorate(TextDecoration.BOLD, TextDecoration.UNDERLINED));
+        meta.lore(List.of(Component.text("A Sword which came from the world of \\\\\"Minceraft\\\\")));
+
+        meta.addAttributeModifier(Attribute.ARMOR, new AttributeModifier(Registry.ATTRIBUTE.getKeyOrThrow(Attribute.ARMOR), 10000, AttributeModifier.Operation.ADD_NUMBER));
+        meta.addAttributeModifier(Attribute.ARMOR_TOUGHNESS, new AttributeModifier(Registry.ATTRIBUTE.getKeyOrThrow(Attribute.ARMOR_TOUGHNESS), 10000, AttributeModifier.Operation.ADD_NUMBER));
+        meta.addAttributeModifier(Attribute.ATTACK_SPEED, new AttributeModifier(Registry.ATTRIBUTE.getKeyOrThrow(Attribute.ATTACK_SPEED), 10000, AttributeModifier.Operation.ADD_NUMBER));
+        meta.addAttributeModifier(Attribute.MOVEMENT_SPEED, new AttributeModifier(Registry.ATTRIBUTE.getKeyOrThrow(Attribute.MOVEMENT_SPEED), 10000, AttributeModifier.Operation.ADD_NUMBER));
+        meta.addAttributeModifier(Attribute.KNOCKBACK_RESISTANCE, new AttributeModifier(Registry.ATTRIBUTE.getKeyOrThrow(Attribute.KNOCKBACK_RESISTANCE), 10000, AttributeModifier.Operation.ADD_NUMBER));
+        meta.addAttributeModifier(Attribute.ATTACK_DAMAGE, new AttributeModifier(Registry.ATTRIBUTE.getKeyOrThrow(Attribute.ATTACK_DAMAGE), 10000, AttributeModifier.Operation.ADD_NUMBER));
+        meta.addAttributeModifier(Attribute.FOLLOW_RANGE, new AttributeModifier(Registry.ATTRIBUTE.getKeyOrThrow(Attribute.FOLLOW_RANGE), 10000, AttributeModifier.Operation.ADD_NUMBER));
+        meta.addAttributeModifier(Attribute.LUCK, new AttributeModifier(Registry.ATTRIBUTE.getKeyOrThrow(Attribute.LUCK), 10000, AttributeModifier.Operation.ADD_NUMBER));
+
+        meta.addEnchant(Enchantment.AQUA_AFFINITY, 255, true);
+        meta.addEnchant(Enchantment.BANE_OF_ARTHROPODS, 255, true);
+        meta.addEnchant(Enchantment.BLAST_PROTECTION, 255, true);
+        meta.addEnchant(Enchantment.CHANNELING, 255, true);
+        meta.addEnchant(Enchantment.DEPTH_STRIDER, 255, true);
+        meta.addEnchant(Enchantment.EFFICIENCY, 255, true);
+        meta.addEnchant(Enchantment.FEATHER_FALLING, 255, true);
+        meta.addEnchant(Enchantment.FIRE_PROTECTION, 255, true);
+        meta.addEnchant(Enchantment.FLAME, 255, true);
+        meta.addEnchant(Enchantment.FORTUNE, 255, true);
+        meta.addEnchant(Enchantment.FROST_WALKER, 255, true);
+        meta.addEnchant(Enchantment.IMPALING, 255, true);
+        meta.addEnchant(Enchantment.INFINITY, 255, true);
+        meta.addEnchant(Enchantment.KNOCKBACK, 255, true);
+        meta.addEnchant(Enchantment.LOOTING, 255, true);
+        meta.addEnchant(Enchantment.LOYALTY, 255, true);
+        meta.addEnchant(Enchantment.LUCK_OF_THE_SEA, 255, true);
+        meta.addEnchant(Enchantment.LURE, 255, true);
+        meta.addEnchant(Enchantment.MENDING, 255, true);
+        meta.addEnchant(Enchantment.MULTISHOT, 255, true);
+        meta.addEnchant(Enchantment.PIERCING, 255, true);
+        meta.addEnchant(Enchantment.POWER, 255, true);
+        meta.addEnchant(Enchantment.PROJECTILE_PROTECTION, 255, true);
+        meta.addEnchant(Enchantment.PROTECTION, 255, true);
+        meta.addEnchant(Enchantment.PUNCH, 255, true);
+        meta.addEnchant(Enchantment.QUICK_CHARGE, 255, true);
+        meta.addEnchant(Enchantment.RESPIRATION, 255, true);
+        meta.addEnchant(Enchantment.RIPTIDE, 255, true);
+        meta.addEnchant(Enchantment.SHARPNESS, 255, true);
+        meta.addEnchant(Enchantment.SILK_TOUCH, 255, true);
+        meta.addEnchant(Enchantment.SMITE, 255, true);
+        meta.addEnchant(Enchantment.SOUL_SPEED, 255, true);
+        meta.addEnchant(Enchantment.SWEEPING_EDGE, 255, true);
+        meta.addEnchant(Enchantment.THORNS, 255, true);
+        meta.addEnchant(Enchantment.THORNS, 255, true);
+        meta.addEnchant(Enchantment.UNBREAKING, 255, true);
+
+        meta.setUnbreakable(true);
+
+        meta.addItemFlags(ItemFlag.values());
+
+        item.setItemMeta(meta);
+        return item;
     }
 
 }
