6f90b81c5ad9c677a124155a8542a3b3b888f414
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-04-19 20:18:24 +0200

    allow access to CombatTracker fights, get the fights a player is involved into and added way to check if a player is currently in combat

--- src/main/java/net/chaossquad/mclib/combattracking/CombatTracker.java
+++ src/main/java/net/chaossquad/mclib/combattracking/CombatTracker.java
@@ -1,9 +1,9 @@
 package net.chaossquad.mclib.combattracking;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
 
 /**
  * Combat tracking system.
@@ -21,6 +21,8 @@ public class CombatTracker {
     public CombatTracker() {
         this.playerFights = Collections.synchronizedMap(new HashMap<>());
     }
+
+    // ----- FUNCTIONALITY -----
 
     /**
      * Counts down the expiration countdowns.
@@ -53,7 +55,7 @@ public class CombatTracker {
      * @param damage the amount of damage
      * @return the amount of damage the player who dealt the damage has dealt in total
      */
-    public double onPlayerDamage(UUID player, UUID damager, double damage) {
+    public double onPlayerDamage(@NotNull UUID player, @NotNull UUID damager, double damage) {
         PlayerFight fight = this.playerFights.get(player);
 
         if (fight == null) {
@@ -70,7 +72,51 @@ public class CombatTracker {
      * @param playerId the player who died
      * @return the player fight for using the tracked data
      */
-    public PlayerFight onPlayerDeath(UUID playerId) {
+    public @Nullable PlayerFight onPlayerDeath(@NotNull UUID playerId) {
         return this.playerFights.remove(playerId);
     }
+
+    // ----- INFORMATION -----
+
+    public @NotNull Map<UUID, PlayerFight> getPlayerFights() {
+        return this.playerFights;
+    }
+
+    /**
+     * Returns a PlayerFight without removing it.
+     * @param playerId player uuid
+     * @return player fight or null
+     */
+    public @Nullable PlayerFight getPlayerFight(@NotNull UUID playerId) {
+        return this.playerFights.get(playerId);
+    }
+
+    /**
+     * Returns all fights the player is involved to.
+     * @param playerId player id
+     * @return list of fights
+     */
+    public @NotNull List<PlayerFight> getInvolvedFights(@NotNull UUID playerId) {
+        List<PlayerFight> involvedFights = new ArrayList<>();
+
+        for (Map.Entry<UUID, PlayerFight> entry : this.playerFights.entrySet()) {
+
+            if (entry.getKey().equals(playerId) || entry.getValue().getStats().containsKey(playerId)) {
+                involvedFights.add(entry.getValue());
+            }
+
+        }
+
+        return involvedFights;
+    }
+
+    /**
+     * Returns true if the specified player has an ongoing fight.
+     * @param playerId player uuid
+     * @return player in combat
+     */
+    public boolean isInCombat(@NotNull UUID playerId) {
+        return !this.getInvolvedFights(playerId).isEmpty();
+    }
+
 }
