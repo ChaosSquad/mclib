1fc58bb08e9ce015165a27107aab744b4ec7e8d1
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-04-26 17:18:52 +0200

    moved over AdvancedGUIUtils#getGUIOriginLocation, AdvancedGUIUtils#guiPointToWorldLocation and AdvancedGUIUtils#canWallGuiBeInteractPhysically from CC2

--- src/main/java/net/chaossquad/mclib/AdvancedGUIUtils.java
+++ src/main/java/net/chaossquad/mclib/AdvancedGUIUtils.java
@@ -1,8 +1,13 @@
 package net.chaossquad.mclib;
 
+import me.leoko.advancedgui.utils.GuiPoint;
+import me.leoko.advancedgui.utils.GuiWallInstance;
 import me.leoko.advancedgui.utils.components.Component;
 import me.leoko.advancedgui.utils.components.GroupComponent;
 import me.leoko.advancedgui.utils.interactions.Interaction;
+import org.bukkit.entity.Player;
+import org.bukkit.util.RayTraceResult;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
@@ -70,6 +75,81 @@ public final class AdvancedGUIUtils {
             return false;
         }
 
+    }
+
+    /**
+     * Checks if the player can interact with the gui. It uses the vanilla interaction radius for that.<br/>
+     * This is necessary because AdvancedGUI does not check if a player is near enough to interact with the gui.
+     * @param instance gui instance the player has interacted with
+     * @param point the point where the player has clicked
+     * @param player the player that has interacted
+     * @param checkForBlocks if this method should do a raytrace to check if blocks are between the player and the gui
+     * @return true = player can interact
+     */
+    public static boolean canWallGuiBeInteractPhysically(@NotNull GuiWallInstance instance, @NotNull GuiPoint point, @NotNull Player player, boolean checkForBlocks) {
+
+        Vector interactedLocation = guiPointToWorldLocation(instance, point);
+        Vector playerLocation = player.getEyeLocation().toVector().clone();
+
+        double distance = interactedLocation.distance(playerLocation);
+
+        double maxDistance;
+        switch (player.getGameMode()) {
+            case SURVIVAL, ADVENTURE -> maxDistance = 4.5;
+            case CREATIVE -> maxDistance = 5.0;
+            default -> maxDistance = 0;
+        }
+
+        if (distance > maxDistance) {
+            return false;
+        }
+
+        if (checkForBlocks) {
+            RayTraceResult result = player.getWorld().rayTraceBlocks(player.getEyeLocation().clone(), player.getEyeLocation().getDirection().clone(), distance);
+            if (result != null) return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Converts the pixel location on a wall gui to the location in the world.
+     * @param instance gui instance
+     * @param point location on the gui
+     * @return location in the world
+     */
+    public static @NotNull Vector guiPointToWorldLocation(@NotNull GuiWallInstance instance, @NotNull GuiPoint point) {
+
+        // click point and screen sizes
+        double x = point.getX();
+        double y = point.getY();
+        double screenMaxX = instance.getLayout().getSize().getWidth() * 128d;
+        double screenMaxY = instance.getLayout().getSize().getHeight() * 128d;
+
+        // Direction vectors
+        Vector xDir = instance.getLocation().getDirection().getXDirection();
+        Vector yDir = instance.getLocation().getDirection().getYDirection();
+
+        // Size
+        Vector worldWidth = xDir.clone().multiply(instance.getLayout().getSize().getWidth());
+        Vector worldHeight = yDir.clone().multiply(instance.getLayout().getSize().getHeight());
+
+        // World relative position
+        Vector relativeWorldPositionX = worldWidth.clone().multiply(x / screenMaxX);
+        Vector relativeWorldPositionY = worldHeight.clone().multiply(y / screenMaxY);
+        Vector relativeWorldPosition = relativeWorldPositionX.clone().add(relativeWorldPositionY);
+
+        // World absolute position
+        return getGUIOriginLocation(instance).add(relativeWorldPosition);
+    }
+
+    /**
+     * Returns the location vector of the GUI coordinate system origin in world coordinates.
+     * @param instance gui instance
+     * @return origin
+     */
+    public static @NotNull Vector getGUIOriginLocation(@NotNull GuiWallInstance instance) {
+        return instance.getLocation().toVector().clone().add(instance.getLocation().getDirection().getGuiSupportVector());
     }
 
 }
