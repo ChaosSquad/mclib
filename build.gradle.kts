6797fa0c82bd2b73b78ae6f7c65b3dc6f8acf346
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-05-03 13:03:56 +0200

    removed AdvancedGUI stuff because it is now in mclib-advancedgui

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,6 @@ repositories {
         url = uri("https://repo.papermc.io/repository/maven-public/")
     }
     maven {
-        name = "respark-releases"
-        url = uri("https://maven.respark.dev/releases")
-    }
-    maven {
         name = "jitpack"
         url = uri("https://jitpack.io")
     }
@@ -32,7 +28,6 @@ paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArt
 dependencies {
     paperweight.paperDevBundle("1.21.4-R0.1-SNAPSHOT")
     compileOnly("org.json:json:20240303")
-    compileOnly("me.leoko.advancedgui:AdvancedGUI:2.2.9")
     compileOnly("net.luckperms:api:5.4")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
     compileOnly("me.clip:placeholderapi:2.11.6")
