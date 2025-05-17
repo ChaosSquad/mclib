92a169fd549aa8252e7010ada5beb12c781761d7
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-05-17 14:43:15 +0200

    moved packet classes over to mclib-packets

--- src/main/java/net/chaossquad/mclib/packetentity/PacketEntityData.java
+++
@@ -1,9 +0,0 @@
-package net.chaossquad.mclib.packetentity;
-
-/**
- * Represents custom data stored in the {@link PacketEntity}
- * @param data data
- * @param <T> data type
- */
-public record PacketEntityData<T>(T data) {
-}
