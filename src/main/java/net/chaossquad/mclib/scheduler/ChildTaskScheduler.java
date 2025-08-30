c8d51f336abae6d503064fc1a6bb49631312cb34
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-08-30 14:18:44 +0200

    removed parent from ChildTaskScheduler that users can register the ChildTaskScheduler whenever they like to another TaskScheduler

--- src/main/java/net/chaossquad/mclib/scheduler/ChildTaskScheduler.java
+++ src/main/java/net/chaossquad/mclib/scheduler/ChildTaskScheduler.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 
 /**
  * A task scheduler that uses another task scheduler as parent.<br/>
+ * It can be registered as a task to another TaskScheduler.<br/>
  * The {@link net.chaossquad.mclib.commands.TasksSubcommand} recognizes this type of tasks and makes it possible to manage them.
  */
 public class ChildTaskScheduler extends TaskScheduler implements TaskRunnable {
@@ -16,11 +17,9 @@ public class ChildTaskScheduler extends TaskScheduler implements TaskRunnable {
      * Creates a task scheduler.
      *
      * @param logger logger
-     * @param parent the task scheduler that is used for tasks
      */
-    public ChildTaskScheduler(@NotNull Logger logger, @NotNull SchedulerInterface parent, @Nullable Removable removeCondition, @NotNull String name) {
+    public ChildTaskScheduler(@NotNull Logger logger, @Nullable Removable removeCondition, @NotNull String name) {
         super(logger);
-        parent.scheduleRepeatingTask(this, 1, 1, removeCondition, "child_scheduler_" + name);
     }
 
     /**
@@ -28,8 +27,8 @@ public class ChildTaskScheduler extends TaskScheduler implements TaskRunnable {
      *
      * @param logger logger
      */
-    public ChildTaskScheduler(@NotNull Logger logger, @NotNull SchedulerInterface parent, @NotNull String name) {
-        this(logger, parent, null, name);
+    public ChildTaskScheduler(@NotNull Logger logger, @NotNull String name) {
+        this(logger, null, name);
     }
 
     @Override
