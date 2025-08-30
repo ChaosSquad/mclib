efefa9980797b7ece229d498a2825d9fed4e1188
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-08-30 12:15:49 +0200

    fixed DataStorage dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,14 @@ repositories {
     maven {
         url = uri("https://repo.extendedclip.com/releases/")
     }
+    maven {
+        name = "chaossquad-releases"
+        url = uri("https://maven.chaossquad.net/releases")
+    }
+    maven {
+        name = "chaossquad-snapshots"
+        url = uri("https://maven.chaossquad.net/snapshots")
+    }
 }
 
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
@@ -30,7 +38,7 @@ dependencies {
     compileOnly("net.luckperms:api:5.4")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
     compileOnly("me.clip:placeholderapi:2.11.6")
-    implementation("net.jandie1505:DataStorage:1.0-SNAPSHOT")
+    implementation("net.jandie1505:DataStorage:1.0-RELEASE")
 }
 
 java {
